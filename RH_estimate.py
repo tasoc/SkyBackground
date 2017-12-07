3b0feb54f7a65368bc4f92e25061b397ed2d99dc
Oliver Hall <ojh251@student.bham.ac.uk>
2017-12-07 17:42:11 +0100

    Changed RH_esitmate.py to an importable function, with a __main__ for local testing.

--- RH_estimate.py
+++ RH_estimate.py
@@ -8,27 +8,79 @@ import numpy as np
 import astropy.io.fits as pyfits
 from photutils import Background2D, SigmaClip, SExtractorBackground
 
-def fit_background(fname):
+'''
+Estimate the background of a Full Frame Image (FFI) using the 'photutils' package.
 
-	# Load file:
-	if fname.endswith('.npy'):
-		img = np.load(fname)
-	else:
-		with pyfits.open(fname, memmap=True, mode='readonly') as hdu:
-			img = hdu[0].data
+Includes a '__main__' for independent test runs on local machines.
 
+Parameters
+-----------
+ffi : float, numpy array
+        A single TESS Full Frame Image in the form of a 2D array.
+-----------
+
+Output
+-----------
+bkg.background : float, numpy array
+        A background estimate in the same style and shape as the FFI input.
+
+TO DO:
+-   Write own Sigma Clip not dependent on the package, which doesnt work
+    on all machines.
+
+'''
+
+def fit_background(ffi):
 	# Create mask
 	# TODO: Use the known locations of bright stars
-	mask = ~np.isfinite(img)
-	mask |= (img > 2e5)
+	mask = ~np.isfinite(ffi)
+	mask |= (ffi > 2e5)
 
 	# Estimate the background:
 	sigma_clip = SigmaClip(sigma=3.0, iters=5)
 	bkg_estimator = SExtractorBackground()
-	bkg = Background2D(img, (64, 64),
+	bkg = Background2D(ffi, (64, 64),
 		filter_size=(3, 3),
 		sigma_clip=sigma_clip,
 		bkg_estimator=bkg_estimator,
 		mask=mask)
 
-	return bkg.background, mask
+	return bkg.background
+
+
+if __name__ == '__main__':
+    plt.close('all')
+
+    # Load file:
+    ffis = ['ffi_north', 'ffi_south', 'ffi_cluster']
+    ffi = ffis[2]
+    sfile = glob.glob('../data/'+ffi+'/simulated/*.fits')[0]
+    bgfile = glob.glob('../data/'+ffi+'/backgrounds.fits')[0]
+
+    try:
+        hdulist = pyfits.open(sfile)
+        bkglist = pyfits.open(bgfile)
+
+    except IOError:
+        print('File not located correctly.')
+        exit()
+
+    ffi = hdulist[0].data
+    bkg = bkglist[0].data
+
+    est_bkg = fit_background(ffi)
+
+
+    '''Plotting: all'''
+    fig, ax = plt.subplots()
+    im = ax.imshow(np.log10(ffi),cmap='Blues_r', origin='lower')
+    fig.colorbar(im,label=r'$log_{10}$(Flux)')
+    ax.set_title(ffi)
+
+    fdiff, adiff = plt.subplots()
+    diff = adiff.imshow(np.log10(est_bkg) - np.log10(ffi), origin='lower')
+    fdiff.colorbar(diff, label='Estimated Bkg - True Bkg (both in log10 space)')
+    adiff.set_title('Estimated bkg - True bkg')
+
+    plt.show('all')
+    plt.close('all')
