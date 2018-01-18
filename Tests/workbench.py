513cdc4cb4e6480d221399cea6bc3510fff96e1a
Oliver Hall <ojh251@student.bham.ac.uk>
2018-01-18 09:18:57 +0100

    Made some minor syntax fixes and added a Filter_estimate method.

--- Tests/workbench.py
+++ Tests/workbench.py
@@ -2,36 +2,47 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 if __name__ == '__main__':
+
+
+
+
+
+
+
+    sys.exit()
     '''My data goes from 0 to 10
     We want first and last 20% superspaced
 
     '''
-    lenx = 10
-    leny = 10
+    xlen = 2048     #Pixels in x and y
+    ylen = 2048
+    perc = 0.2     #Percentage of image to be double spaced
+    nside = 10     #Number of points per side without increased density
 
-    superx = 0.1*10
-    supery = 0.1*10
+    lx = xlen/(nside+2)
+    ly = ylen/(nside+2)
 
-    nsuper = 7 #Number of supergrid points
-    nreg = 14
+    superx = lx/2
+    supery = ly/2
 
-    lx = superx/(nsuper+2)
-    ly = supery/(nsuper+2)
+    nsuper = perc*nside*2
+    nreg = (1-2*perc)*nside
 
-    xlocs_left = np.linspace(lx, superx-lx, nsuper)
-    ylocs_left = np.linspace(ly, supery-ly, nsuper)
+    xx = perc*xlen
+    yy = perc*xlen
 
-    xlocs_right = np.linspace(lenx-superx+lx, lenx-lx, nsuper)
-    ylocs_right = np.linspace(leny-supery+ly, leny-ly, nsuper)
+    xlocs_left = np.linspace(superx, xx-superx, nsuper)
+    ylocs_left = np.linspace(supery, yy-supery, nsuper)
 
-    xlocs_mid = np.linspace(superx,lenx-superx,nreg)
-    ylocs_mid = np.linspace(supery,leny-supery,nreg)
+    xlocs_right = np.linspace(xlen-xx+superx, xlen-superx, nsuper)
+    ylocs_right = np.linspace(ylen-yy+supery, ylen-supery, nsuper)
+
+    xlocs_mid = np.linspace(xx,xlen-xx,nreg)
+    ylocs_mid = np.linspace(yy,ylen-yy,nreg)
 
 
-    xx = np.append(xlocs_left, xlocs_mid)
-    xx = np.append(xx, xlocs_right)
-    yy = np.append(ylocs_left, ylocs_mid)
-    yy = np.append(yy, ylocs_right)
+    xx = np.append(xlocs_mid, [xlocs_left, xlocs_right])
+    yy = np.append(ylocs_mid, [ylocs_left,ylocs_right])
     X, Y = np.meshgrid(xx, yy)
 
     plt.scatter(X, Y, s=1)
